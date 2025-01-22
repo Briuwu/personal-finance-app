8b55b8c10ab5e19a963a6a01feeb5342b39acf53
Briuwu <millontebrian@gmail.com>
2025-01-22 09:17:24 +0800

    Refactor database schema: update users table and add relations for transactions, budgets, and pots

--- db/schema.ts
+++ db/schema.ts
@@ -1,27 +1,80 @@
-import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import {
+  boolean,
+  numeric,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users_table", {
+export const users = pgTable("users", {
   id: serial("id").primaryKey(),
+  username: text("name").notNull(),
+  userId: text("user_id").notNull(),
+  createdAt: timestamp("created_at").notNull(),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  transactions: many(transactions),
+  budgets: many(budgets),
+  pots: many(pots),
+}));
+
+export const transactions = pgTable("transactions", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  category: text("category").notNull(),
   name: text("name").notNull(),
-  age: integer("age").notNull(),
-  email: text("email").notNull().unique(),
-});
-
-export const postsTable = pgTable("posts_table", {
-  id: serial("id").primaryKey(),
-  title: text("title").notNull(),
-  content: text("content").notNull(),
-  userId: integer("user_id")
-    .notNull()
-    .references(() => usersTable.id, { onDelete: "cascade" }),
+  avatar: text("avatar").notNull(),
+  amount: numeric("amount", { precision: 100, scale: 2 }).notNull(),
+  recurring: boolean("recurring").notNull(),
+  date: timestamp("date").notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at")
-    .notNull()
-    .$onUpdate(() => new Date()),
 });
 
-export type InsertUser = typeof usersTable.$inferInsert;
-export type SelectUser = typeof usersTable.$inferSelect;
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  user: one(users, {
+    fields: [transactions.userId],
+    references: [users.id],
+  }),
+}));
 
-export type InsertPost = typeof postsTable.$inferInsert;
-export type SelectPost = typeof postsTable.$inferSelect;
+export const budgets = pgTable("budgets", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  category: text("category").notNull(),
+  maximum: numeric("maximum", { precision: 100, scale: 2 }).notNull(),
+  theme: text("theme").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const budgetsRelations = relations(budgets, ({ one }) => ({
+  user: one(users, {
+    fields: [budgets.userId],
+    references: [users.id],
+  }),
+}));
+
+export const pots = pgTable("pots", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  name: text("name").notNull(),
+  target: numeric("target", { precision: 100, scale: 2 }).notNull(),
+  total: numeric("total", { precision: 100, scale: 2 }).notNull(),
+  theme: text("theme").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const potsRelations = relations(pots, ({ one }) => ({
+  user: one(users, {
+    fields: [pots.userId],
+    references: [users.id],
+  }),
+}));
